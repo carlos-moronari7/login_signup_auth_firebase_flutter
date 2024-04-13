3c9c4b5d640112970ed3bf7e66d90e4d1754baa4
Seu Nome <carlos.moronari7@gmail.com>
2024-04-13 20:39:35 +0100

    First two textboxes

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
 
-class LoginPage extends StatelessWidget{
-    const LoginPage({super.key});
+import 'package:flutter_signup_auth_firebase_crud/components/my_textfield.dart';
+
+class LoginPage extends StatelessWidget {
+   LoginPage({super.key});
+
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
-  Widget build(BuildContext context){
-    return Scaffold();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children:  [
+              const SizedBox(
+                height: 50,
+              ),
+              //logo
+              const Icon(
+                Icons.lock,
+              size:100,),
+
+              const SizedBox(
+                height: 50,
+              ),
+              Text(
+                "Welcome back! You\'ve been missed!",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16
+                ),
+              ),
+               const SizedBox(
+                height: 25,
+              ),
+             MyTextField(
+              controller: usernameController,
+              hintText: "Username",
+              obscureText: false,),
+
+            const SizedBox(
+                height: 25,
+              ),
+              MyTextField(
+                controller: passwordController,
+                hintText: "Password",
+                obscureText: true,),
+
+              
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
